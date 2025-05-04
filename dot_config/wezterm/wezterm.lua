b21510782eca4b49fd263d6d3021e402d8906ec2
Delta-Pion <adi.28per@gmail.com>
2025-05-04 23:20:16 +0530

    Wezterm theme made sexier

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,10 +6,44 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
---config.color_scheme = '3024 (dark) (terminal.sexy)'
+-- For example, changing the COLOR SCHEME:
+-- config.color_scheme = '3024 (dark) (terminal.sexy)'
+-- config.color_scheme = "Tomorrow Night Burns"
+--
 
-config.color_scheme = "Tomorrow Night Burns"
+local neofusion_theme = {
+	foreground = "#e0d9c7",
+	background = "#070f1c",
+	cursor_bg = "#e0d9c7",
+	cursor_border = "#e0d9c7",
+	cursor_fg = "#070f1c",
+	selection_bg = "#ea6847",
+	selection_fg = "#e0d9c7",
+	ansi = {
+		"#070f1c", -- Black (Host)
+		"#dd0000", -- Red (Syntax string)
+		"#dd0000", -- Green (Command)
+		"#5db2f8", -- Yellow (Command second)
+		"#2f516c", -- Blue (Path)
+		"#d943a8", -- Magenta (Syntax var)
+		"#86dbf5", -- Cyan (Prompt)
+		"#e0d9c7", -- White
+	},
+	brights = {
+		"#2f516c", -- Bright Black
+		"#d943a8", -- Bright Red (Command error)
+		"#dd0000", -- Bright Green (Exec)
+		"#86dbf5", -- Bright Yellow
+		"#5db2f8", -- Bright Blue (Folder)
+		"#d943a8", -- Bright Magenta
+		"#dd0000", -- Bright Cyan
+		"#e0d9c7", -- Bright White
+	},
+}
+
+config.colors = neofusion_theme
+-- "#ea6847", -- Bright Cyan (original neofusino color for red)
+
 config.font = wezterm.font("Iosevka SS04")
 config.font_size = 14.5
 config.default_prog = { "pwsh" }
